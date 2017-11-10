87b182d24869e4f1ce608b5477a8b16c9a1e9f3f
mathewleland <mathewhleland@gmail.com>
2017-11-09 20:03:10 -0800

    initialize app

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,119 @@
 import React, { Component } from 'react';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd/modules/backends/HTML5';
-
-import urls from '../constants/urls';
 import GifSwatch from './GifSwatch';
 import GifDropzone from './GifDropzone';
-import mockResponse from '../constants/mockResponse';
+import axios from 'axios';
 
 import './App.scss';
 
 @DragDropContext(HTML5Backend)
 export class App extends Component {
 
+  constructor() {
+    super()
+
+    this.state = {
+      searchResults: [],
+      query: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   renderSwatch(r) {
     const props = {
       key: r.id,
       id: r.id,
-      thumbnailUrl: r.images.fixed_height_small_still.url,
+      thumbnailUrl: r.images.fixed_height_still.url,
+      playingUrl: r.images.fixed_height.url,
+      previewUrl: r.images.preview_gif.url,
     };
     return (<GifSwatch {...props} />);
   }
 
-  render() {
-    let logoUrl = require('../../static/images/logo.svg');
+  componentDidMount() {
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=25')
+         .then( res => {
+           this.setState({ searchResults: res.data.data});
+         })
+         .catch( err => {
+           console.log(err);
+         });
+  }
 
-    // TODO connect results to search box and giphy API
-    let resultSwatches = mockResponse.data.map(::this.renderSwatch);
+  searchGifs() {
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=25&q=${this.state.query}`)
+      .then( res => {
+        this.setState({ searchResults: res.data.data});
+      })
+      .catch( err => {
+        console.log(err);
+      });
+  }
+
+  handleChange(e) {
+    this.setState({ query: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.searchGifs();
+  }
+
+  render() {
+
+    let resultSwatches = this.state.searchResults.map(::this.renderSwatch);
     return (
       <div className="App">
         <header className="masthead">
           <h1>
-            <a href="/">
-              <img src={logoUrl} height={36} />
-              &nbsp;
-              <span>Gifs</span>
-            </a>
+            My Day in GIFs
           </h1>
+          <p> Grab a GIF and drag it to a box </p>
+          <p> Powered By <a href='https://developers.giphy.com/'>GIPHY </a></p>
         </header>
         <div className="content-container">
-          <main className="main">
-            <GifDropzone src="https://media.giphy.com/media/13p77tfexyLtx6/giphy.gif"/>
-            <GifDropzone src="https://media.giphy.com/media/iLqpYAbKGOrqU/giphy.gif"/>
-            <GifDropzone src="https://media.giphy.com/media/8ytDUrlW9JbG0/giphy.gif"/>
-            <GifDropzone src="https://media.giphy.com/media/PekRU0CYIpXS8/giphy.gif"/>
+
+          <aside className="search">
+            <form onSubmit={this.handleSubmit}>
+              <input 
+                className="search-input" 
+                name="search" 
+                placeholder="Find the perfect GIF" 
+                onChange={this.handleChange} 
+                value={this.state.query}/>
+            </form>
+            <div className='gifResults'>
+              { resultSwatches }
+            </div>
+        </aside>
+
+          <main className="dropped">
+            <div className='dropped-item'>
+              <h3> How I imagined it would go</h3>
+              <GifDropzone />
+            </div>
+            <div className='dropped-item'>
+              <h3> How it started out </h3>
+              <GifDropzone />
+            </div>
+            <div className='dropped-item'>
+            <h3> What it immediately turned into</h3>
+            <GifDropzone />
+            </div>
+            <div className='dropped-item'>
+              <h3> Left me like </h3>
+              <GifDropzone />
+            </div>
+            
+            
+            
           </main>
-          <aside className="sidebar">
-            <input className="search-input" name="search" placeholder="Search" />
-            { resultSwatches }
-          </aside>
+          
         </div>
-        <footer className="footer">
-          {<a href={urls.claraHomepage} target="_blank">About Clara</a>}
-          {' | '}
-          {<a href={urls.claraCareers} target="_blank">Work at Clara</a>}
-        </footer>
+        
       </div>
     );
   }
